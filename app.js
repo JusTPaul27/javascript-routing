135da3f88a23cf4a1c8231cf6658a97bd4d13477
Paolo Foppiano <103486420+JusTPaul27@users.noreply.github.com>
2022-06-01 11:33:09 +0200

    fine lezione

---
+++ app.js
@@ -0,0 +1,60 @@
+let pages = []
+
+function loadedPages() {
+    fetch("./pages.json")
+    .then(response => response.json())
+    .then(result => displayPages(result));
+}
+
+
+
+function displayPages(loadedPages) {
+
+    pages = loadedPages;
+    const navMenu = document.getElementById('nav-menu');
+
+    for (const page of pages) {
+        console.log(page);
+        const a = document.createElement('a');
+        const node = document.createTextNode(page.name);
+        a.appendChild(node);
+        a.href = "/#/" + page.id;
+        navMenu.appendChild(a);
+    }
+
+    changePage(window.location.hash);
+
+    // const firstPage = pages[0];
+
+    // const container = document.getElementById("page-container");
+
+    // container.innerHTML = firstPage.html;
+}
+
+window.addEventListener('hashchange', () => changePage(window.location.hash));
+
+
+function changePage(hash) {
+
+let id = hash.replace('#/', '');  
+
+if (id === '') {
+    id = 'home';
+}
+
+let selectedPage;
+
+for (const page of pages) {
+    if (page.id === id) {
+        selectedPage = page;
+        break;
+    }
+}
+
+const container = document.getElementById("page-container");
+
+container.innerHTML = selectedPage.html;
+}
+
+
+loadedPages();
